e014d9b506cfc241711be257f4515e8a5e3766aa
Andrés Isaza <andres.isaza1506@gmail.com>
2020-04-09 19:24:27 -0500

    resized images, changed speed and window size

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import cv2
 import numpy as np
 # Global Variables for the game
 FPS = 30
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 520
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 500
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 GROUNDY = SCREEN_HEIGHT
 GAME_SPRITES = {}
@@ -17,11 +17,10 @@ BACKGROUND = 'gallery/sprites/FONDOMURAL.jpg'
 PIPE = 'gallery/sprites/columna.png'
 captura = cv2.VideoCapture(0)
 
+
 def welcomeScreen():
     player_x = int(SCREEN_WIDTH / 5)
     player_y = int((SCREEN_HEIGHT - GAME_SPRITES['player'].get_height()) / 2)
-    messagey = int(SCREEN_HEIGHT * 0.13)
-    # base_x = 0
 
     while True:
         for event in pygame.event.get():
@@ -31,7 +30,7 @@ def welcomeScreen():
                 sys.exit()
 
             # If the user presses space or up key, start the game for them
-            elif event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP): 
+            elif event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
                 return
             else:
                 SCREEN.blit(GAME_SPRITES['background'], (0, 0))
@@ -44,7 +43,6 @@ def mainGame():
     score = 0
     player_x = int(SCREEN_WIDTH / 5)
     player_y = int(SCREEN_WIDTH / 2)
-    base_x = 0
 
     # Create 2 pipes for blitting on the screen
     newPipe1 = getRandomPipe()
@@ -52,27 +50,20 @@ def mainGame():
 
     # my List of upper pipes
     upper_pipes = [
-        {'x': SCREEN_WIDTH + 200, 'y': newPipe1[0]['y']},
-        {'x': SCREEN_WIDTH + 200 + (SCREEN_WIDTH / 2), 'y': newPipe2[0]['y']}
+        {'x': SCREEN_WIDTH + 150, 'y': newPipe1[0]['y']},
+        {'x': SCREEN_WIDTH + 150 + (SCREEN_WIDTH / 2), 'y': newPipe2[0]['y']}
     ]
     # my List of lower pipes
     lower_pipes = [
-        {'x': SCREEN_WIDTH + 200, 'y': newPipe1[1]['y']},
-        {'x': SCREEN_WIDTH + 200 + (SCREEN_WIDTH / 2), 'y': newPipe2[1]['y']}
+        {'x': SCREEN_WIDTH + 150, 'y': newPipe1[1]['y']},
+        {'x': SCREEN_WIDTH + 150 + (SCREEN_WIDTH / 2), 'y': newPipe2[1]['y']}
     ]
 
-    pipe_vel_x = -4
-    player_vel_y = -9
-    player_vel_x = 10
-    player_max_vel_y = 10
-    player_min_vel_y = -8
-    player_acc_y = 1
-
-    playerFlapAccv = -10  # velocity while flapping
-    playerFlapped = False  # It is true only when the bird is flapping
+    pipe_vel_x = -8
+    player_vel_y = 5
 
     while True:
-       
+
         print(f'X: {player_x}, Y: {player_y}')
         _, imagen = captura.read()
         hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
@@ -85,12 +76,12 @@ def mainGame():
     # Crear una mascara con solo los pixeles dentro del rango de azuls
         mask = cv2.inRange(hsv, azul_bajos, azul_altos)
 
-          # Encontrar el area de los objetos que detecta la camara
+        # Encontrar el area de los objetos que detecta la camara
         moments = cv2.moments(mask)
         area = moments['m00']
         if(area > 2000000):
 
-        # Buscamos el centro x, y del objeto
+            # Buscamos el centro x, y del objeto
             x = int(moments['m10']/moments['m00'])
             player_y = int(moments['m01']/moments['m00'])
 
@@ -99,10 +90,11 @@ def mainGame():
             print("y = ", player_y)
 
         # Dibujamos una marca en el centro del objeto
-            cv2.rectangle(imagen, (x, player_y), (x+2, player_y+2), (0, 0, 255), 2)
-        
-        # cv2.imshow('mask', mask)   
-        # cv2.imshow('Camara', imagen)    
+            cv2.rectangle(imagen, (x, player_y),
+                          (x+2, player_y+2), (0, 0, 255), 2)
+
+        # cv2.imshow('mask', mask)
+        # cv2.imshow('Camara', imagen)
         is_crashed = isCollide(player_x, player_y, upper_pipes, lower_pipes)
         if is_crashed:
             return
@@ -114,12 +106,8 @@ def mainGame():
             if pipeMidPos <= playerMidPos < pipeMidPos + 4:
                 score += 1
                 GAME_SOUNDS['point'].play()
-        if player_vel_y < player_max_vel_y and not playerFlapped:
-            player_vel_y += player_acc_y
-        if playerFlapped:
-            playerFlapped = False
         playerHeight = GAME_SPRITES['player'].get_height()
-        # player_y = player_y + min(player_vel_y, GROUNDY - player_y - playerHeight)
+        player_y = player_y + min(player_vel_y, GROUNDY - player_y - playerHeight)
 
         # move pipes to the left
         for upper_pipe, lower_pipe in zip(upper_pipes, lower_pipes):
@@ -127,7 +115,7 @@ def mainGame():
             lower_pipe['x'] += pipe_vel_x
 
         # Add a new pipe when the first is about to cross the leftmost part of the screen
-        if 0 < upper_pipes[0]['x'] < 5:
+        if 0 < upper_pipes[0]['x'] < abs(pipe_vel_x) + 1:
             newpipe = getRandomPipe()
             upper_pipes.append(newpipe[0])
             lower_pipes.append(newpipe[1])
@@ -145,7 +133,6 @@ def mainGame():
             SCREEN.blit(GAME_SPRITES['pipe'][1],
                         (lower_pipe['x'], lower_pipe['y']))
 
-        # SCREEN.blit(GAME_SPRITES['base'], (base_x, GROUNDY))
         SCREEN.blit(GAME_SPRITES['player'], (player_x, player_y))
         myDigits = [int(x) for x in list(str(score))]
         width = 0
@@ -162,7 +149,7 @@ def mainGame():
 
 
 def isCollide(player_x, player_y, upper_pipes, lower_pipes):
-    if player_y >= GROUNDY - 25 or player_y < 0:
+    if player_y >= GROUNDY - 40 or player_y < 0:
         GAME_SOUNDS['hit'].play()
         return True
     for pipe in upper_pipes:
@@ -179,7 +166,7 @@ def isCollide(player_x, player_y, upper_pipes, lower_pipes):
 
 def getRandomPipe():
     pipe_height = GAME_SPRITES['pipe'][0].get_height()
-    offset = SCREEN_HEIGHT / 3
+    offset = SCREEN_HEIGHT / 4
     y2 = offset + random.randrange(0, int(SCREEN_HEIGHT - 1.2 * offset))
     pipe_x = SCREEN_WIDTH + 10
     y1 = pipe_height - y2 + offset
